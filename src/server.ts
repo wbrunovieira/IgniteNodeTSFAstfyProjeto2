9fe35e55e2c3f36aa7a1dcb79c0626de885e632a
wbrunovieira <wbrunovieira77@gmail.com>
2024-03-02 20:46:16 -0300

    added typescript fastyfi an start server

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import fastify from 'fastify';
+
+const app = fastify();
+
+app.get('/', async (request, reply) => {
+  return 'Boa Brunao';
+});
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('Server is running on port 3333');
+  });
