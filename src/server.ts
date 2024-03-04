f955c1c2af347435d0cd902ea328a261012fcd41
wbrunovieira <wbrunovieira77@gmail.com>
2024-03-04 19:51:24 -0300

    added .env and test connections

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 import fastify from 'fastify';
+import crypto from 'node:crypto';
 import { knex } from '../database';
 
 const app = fastify();
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*');
-  return tables;
+  const transactions = await knex('transactions').insert({
+    id: crypto.randomUUID(),
+    title: 'Salary',
+    amount: 1000,
+  });
+  return transactions;
 });
 
 app
